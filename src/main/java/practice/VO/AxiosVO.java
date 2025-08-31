ccba0d84163109135007f01f059f9a489f6f8ca3
pshingsl <w455ok@gmail.com>
2025-09-01 00:39:58 +0900

    스프링 동적 폼 실습

---
+++ src/main/java/practice/VO/AxiosVO.java
@@ -0,0 +1,13 @@
+package practice.VO;
+
+import lombok.Getter;
+
+@Getter
+public class AxiosVO {
+    private String name;
+    private String gen;
+    private String year;
+    private String month;
+    private String day;
+    private String interest;
+}
