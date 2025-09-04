a4ac37cac625bb46c4db085121c95aa7bb6f26b5
pshingsl <w455ok@gmail.com>
2025-09-04 15:37:37 +0900

    스프링 중요 어노테이션

--- src/main/java/practice/Application.java
+++ src/main/java/practice/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	// 스프링 부트 시작점
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
